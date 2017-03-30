b84624162afdee69d65f10db9d2a634b0be8190b
Jennifer Kinsey and Nick Powell <jennifer@>
2017-03-30 10:09:44 -0700

    add function to check if its in the thousands. if so, add the appropriate number of Ms to the roman numberal variable. subtract the appropriate number of 1000s off the input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // -----Business Logic-----
 
 //Function 1: Alert if you don't put in a good #
+var romanNumeral = "";
 
 var isNotTooBig = function(input){
   if(input > 3999){
@@ -10,6 +11,17 @@ var isNotTooBig = function(input){
   }
 }
 
+var isInThousands = function(input){
+  if (input/1000 >= 1){
+    var m = Math.floor(input/1000);
+    input = input - (m * 1000);
+    console.log(input);
+    for(var i = 0; i < m; i++){
+      romanNumeral = romanNumeral + "M";
+    }
+    return input;
+  }
+}
 
 
 
@@ -26,11 +38,14 @@ $(function() {
     console.log(userInput);
 
     var test1 = isNotTooBig(userInput);
-    console.log("The number is not too big: " + test1);
     if(test1 === false){
       $(".alert").append("<li> Your number is too large. Try again. </li>")
       $("p").hide();
     }
+
+    var test2 = isInThousands(userInput);
+    console.log("this is what's left after subtracting thous: " + test2);
+    console.log("this is our roman numn: " + romanNumeral);
 
     $("form").hide();
     $("#result").show();
