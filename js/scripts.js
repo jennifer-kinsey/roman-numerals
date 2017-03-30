2e5f35876aa84f576fab974e0cf445ac1645173d
Jennifer Kinsey and Nick Powell <nick@>
2017-03-30 14:02:42 -0700

    finish the 4 repeat character logic with the correct subtractive format.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,16 @@ var romanNumeral = "";
 var isNotTooBig = function(input){
   if(input > 3999){
     return false;
-  } else{
-    return true;
-  }
+  };
 }
 
 var isInThousands = function(input){
   if (input/1000 >= 1){
     var m = Math.floor(input/1000);
-    input = input - (m * 1000);
+    input -= (m * 1000);
     console.log(input);
     for(var i = 0; i < m; i++){
-      romanNumeral = romanNumeral + "M";
+      romanNumeral += "M";
     }
   }
   return input;
@@ -25,8 +23,8 @@ var isInThousands = function(input){
 
 var isInFiveHundreds = function(input){
   if (input/500 >= 1) {
-    input = input - 500;
-    romanNumeral = romanNumeral + "D";
+    input -= 500;
+    romanNumeral += "D";
   }
   return input;
 }
@@ -34,9 +32,9 @@ var isInFiveHundreds = function(input){
 var isInHundreds = function(input){
   if (input/100 >= 1){
     var c = Math.floor(input/100);
-    input = input - (c * 100);
+    input -= (c * 100);
     for(var i = 0; i < c; i++){
-      romanNumeral = romanNumeral + "C";
+      romanNumeral += "C";
     }
   }
   return input;
@@ -44,8 +42,8 @@ var isInHundreds = function(input){
 
 var isInFifties = function(input){
   if (input/50 >= 1) {
-    input = input - 50;
-    romanNumeral = romanNumeral + "L";
+    input -= 50;
+    romanNumeral += "L";
   }
   return input;
 }
@@ -53,9 +51,9 @@ var isInFifties = function(input){
 var isInTens = function(input){
   if (input/10 >= 1){
     var x = Math.floor(input/10);
-    input = input - (x * 10);
+    input -= (x * 10);
     for(var i = 0; i < x; i++){
-      romanNumeral = romanNumeral + "X";
+      romanNumeral += "X";
     }
   }
   return input;
@@ -63,17 +61,53 @@ var isInTens = function(input){
 
 var isInFives = function(input){
   if (input/5 >= 1) {
-    input = input - 5;
-    romanNumeral = romanNumeral + "V";
+    input -= 5;
+    romanNumeral += "V";
   }
   return input;
 }
 
 var isInTheOnes = function(input){
   for(var i = 0; i < input; i++){
-    romanNumeral = romanNumeral + "I";
+    romanNumeral += "I";
   }
   return 0;
+}
+
+var isFourIs = function(input) {
+  //for the fours
+  if ((input.indexOf("IIII") >= 0) && (input.indexOf("V") < 0)) {
+    romanNumeral = input.replace(/I{4}/g, "IV");
+  }
+  //for the nines
+  if ((input.indexOf("IIII") >= 0) && (input.indexOf("V") >= 0)){
+    romanNumeral = input.replace(/VI{4}/g, "IX");
+  }
+  return romanNumeral;
+}
+
+var isFourXs = function(input) {
+  //for forties
+  if((input.indexOf("XXXX")>=0) && (input.indexOf("L") < 0)){
+    romanNumeral = input.replace(/X{4}/g, "XL");
+  }
+  //for nineties
+  if((input.indexOf("XXXX")>=0) && (input.indexOf("L") >= 0)){
+    romanNumeral = input.replace(/LX{4}/g, "XC");
+  }
+  return romanNumeral;
+}
+
+var isFourCs = function(input) {
+  //for fourHundreds
+  if((input.indexOf("CCCC")>=0) && (input.indexOf("D") < 0)){
+    romanNumeral = input.replace(/C{4}/g, "CD");
+  }
+  //for nineHundreds
+  if((input.indexOf("CCCC")>=0) && (input.indexOf("D") >= 0)){
+    romanNumeral = input.replace(/DC{4}/g, "CM");
+  }
+  return romanNumeral;
 }
 
 
@@ -84,9 +118,7 @@ $(function() {
 
   $("form").submit(function(event) {
     event.preventDefault();
-    var originalInput = $("#userInput").val();
-    var userInput = parseInt(originalInput);
-    console.log(userInput);
+    var userInput = parseInt($("#userInput").val());
 
     var test1 = isNotTooBig(userInput);
     if(test1 === false){
@@ -94,65 +126,45 @@ $(function() {
       $("p").hide();
     }
 
-    console.log(originalInput + ": this is what we typed in");
+    var messyRomanNumeral = isInTheOnes(isInFives(isInTens(isInFifties(isInHundreds(isInFiveHundreds(isInThousands(userInput)))))));
 
-    var test2 = isInThousands(userInput);
-    console.log("this is what's left after subtracting thous: " + test2);
-    console.log("this is our roman numn: " + romanNumeral);
+    messyRomanNumeral = isFourIs(romanNumeral);
+    messyRomanNumeral = isFourXs(romanNumeral);
+    messyRomanNumeral = isFourCs(romanNumeral);
 
-    var test3 = isInFiveHundreds(test2);
-    console.log("this is what's left after subtracting 500: " + test3);
-    console.log("this is our roman numn: " + romanNumeral);
+    $("#romanResult").text(romanNumeral);
 
-    var test4 = isInHundreds(test3);
-    console.log("this is what's left after subtracting hundreds: " + test4);
-    console.log("this is our roman numn: " + romanNumeral);
+    // var test2 = isInThousands(userInput);
+    // console.log("this is what's left after subtracting thous: " + test2);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    // var test3 = isInFiveHundreds(test2);
+    // console.log("this is what's left after subtracting 500: " + test3);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    // var test4 = isInHundreds(test3);
+    // console.log("this is what's left after subtracting hundreds: " + test4);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    // var test5 = isInFifties(test4);
+    // console.log("this is what's left after subtracting hundreds: " + test5);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    //
+    // var test6 = isInTens(test5);
+    // console.log("this is what's left after subtracting tens: " + test6);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    // var test7 = isInFives(test6);
+    // console.log("this is what's left after subtracting fives: " + test7);
+    // console.log("this is our roman numn: " + romanNumeral);
+    //
+    // var test8 = isInTheOnes(test7);
+    // console.log("this is what's left after subtracting ones: " + test8);
+    // console.log("this is our roman numn: " + romanNumeral);
 
-    var test5 = isInFifties(test4);
-    console.log("this is what's left after subtracting hundreds: " + test5);
-    console.log("this is our roman numn: " + romanNumeral);
 
 
-    var test6 = isInTens(test5);
-    console.log("this is what's left after subtracting tens: " + test6);
-    console.log("this is our roman numn: " + romanNumeral);
-
-    var test7 = isInFives(test6);
-    console.log("this is what's left after subtracting fives: " + test7);
-    console.log("this is our roman numn: " + romanNumeral);
-
-    var test8 = isInTheOnes(test7);
-    console.log("this is what's left after subtracting ones: " + test8);
-    console.log("this is our roman numn: " + romanNumeral);
-
-    var isFourIs = function(input) {
-      //for the ones place
-      if (originalInput.charAt(originalInput.length - 1) === "4"){
-        romanNumeral = romanNumeral.substring(0, romanNumeral.length -4) + "IV";
-      }
-      if (originalInput.charAt(originalInput.length - 1) === "9"){
-        romanNumeral = romanNumeral.substring(0, romanNumeral.length -4) + "IX";
-      }
-      return romanNumeral;
-    }
-
-    var isFourXs = function(input) {
-      //for forties
-      if((romanNumeral.indexOf("XXXX")>=0) && (romanNumeral.indexOf("L") < 0)){
-        romanNumeral.replace(/X{4}/g, "XL");
-      }
-      //for nineties
-      if((romanNumeral.indexOf("XXXX")>=0) && (romanNumeral.indexOf("L") >= 0)){
-        romanNumeral.replace(/X{4}/g, "XC");
-      }
-      return romanNumeral;
-    }
-
-    var test9 = isFourIs(originalInput);
-    console.log("this is what's going on if it ends in a 4 or 9: " +  test9);
-
-    var test10 = isFourXs(romanNumeral);
-    console.log("40s and 90s ought to work: " + test10);
 
     $("form").hide();
     $("#result").show();
